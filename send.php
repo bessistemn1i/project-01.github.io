8d81feafafdb5badaa74c6d18bd34538069d69c4
bessistemn1i <bessistemn1i@gmail.com>
2018-09-07 11:54:25 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,10 +1,8 @@
 <?
   $name=$_POST['name'];
-  $email=$_POST['email'];
-  $text=$_POST['message'];
   $recapture=$_POST['g-recaptcha-response'];
-  $msg="Вам прислали сообщение с сайта\nИмя: {$name}\nНомер телефона: {$email}\nСообщение: {$message}\n";
-  mail('eduard.zhulega@yandex.ru','Сообщение с сайта', $msg,'From: ${name}');
+  $msg="Вам прислали сообщение с сайта\nИмя: {$name};
+  mail('eduard.zhulega@yandex.ru','Сообщение с сайта', 'From: ${name}');
   echo "Ваше сообщение успешно отправлено";
   print_r($_POST);
 ?> 
