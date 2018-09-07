45f0d1cd7ca644fa7b61e77442a75fcd460aee5f
bessistemn1i <bessistemn1i@gmail.com>
2018-09-07 12:58:56 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?
   $name=$_POST['name'];
-  $recapture=$_POST['g-recaptcha-response'];
+  //$recapture=$_POST['g-recaptcha-response'];
   mail('eduard.zhulega@yandex.ru','Сообщение с сайта', 'From: ${name}');
   print_r($_POST);
 ?> 
